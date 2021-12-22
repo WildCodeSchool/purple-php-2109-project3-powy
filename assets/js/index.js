0c5e894af65fa70fb8cb3f595986e67e18a1c535
Thomas905 <thomasclarisse03@gmail.com>
2021-12-22 21:56:28 +0100

    fix js file for howto

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+const acc = document.getElementsByClassName('accordion-howto');
+let i;
+for (i = 0; i < acc.length; i + 1) {
+    acc[i].addEventListener('click', function () {
+        this.classList.toggle('active');
+        const panel = this.nextElementSibling;
+        if (panel.style.display === 'block') {
+            panel.style.display = 'none';
+        } else {
+            panel.style.display = 'block';
+        }
+    });
+}
