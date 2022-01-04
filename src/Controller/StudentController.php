442b1ceb3c36527d2fdbe912095d93a0ae0a4640
Thomas905 <thomasclarisse03@gmail.com>
2022-01-04 09:35:29 +0100

    fix etudiant to student

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class StudentController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/student", name="student_index")
      */
-    public function index(): Response
+    public function etudiant(): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('student/index.html.twig');
     }
 }
