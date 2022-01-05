d1a4da9bce48fb5a6d3f5a5c5433f65702ebe5d0
Caroline crepin <crepin.caroline@gmail.com>
2022-01-05 15:49:01 +0100

    us2 resolve fix

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ * @UniqueEntity(fields={"email"}, message="il y a déjà un compte avec cet email")
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
