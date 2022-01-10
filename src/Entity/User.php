398ad9dc70e4711594e95433666a886351e66706
Thomas905 <thomasclarisse03@gmail.com>
2022-01-10 12:10:57 +0100

    fix code quality

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -99,7 +99,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private ?Mentor $mentor;
 
-    /** 
+    /**
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private ?string $picture = null;
@@ -295,7 +295,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function setPicture(?string $picture): self
     {
-       $this->picture = $picture;
+        $this->picture = $picture;
         return $this;
     }
 }
