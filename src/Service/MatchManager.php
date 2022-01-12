57a0928893897eea775839f871aba291a48b55a7
Elodie Daubie <elodie.daubie@gmail.com>
2022-01-12 16:59:55 +0100

    creation of constructor for dependance injection

--- src/Service/MatchManager.php
+++ src/Service/MatchManager.php
@@ -2,7 +2,6 @@
 
 namespace App\Service;
 
-use App\Entity\Mentor;
 use App\Entity\Mentoring;
 use App\Entity\Student;
 use App\Repository\MentorRepository;
@@ -11,18 +10,23 @@ use Exception;
 
 class MatchManager
 {
+    private MentorRepository $mentorRepository;
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(MentorRepository $mentorRepository, EntityManagerInterface $entityManager)
+    {
+        $this->mentorRepository = $mentorRepository;
+        $this->entityManager = $entityManager;
+    }
     /**
      * first version of matching (by professionnal sector only)
      */
-    public function match(
-        Student $studentToMatch,
-        MentorRepository $mentorRepository,
-        EntityManagerInterface $entityManager
-    ): void {
+    public function match(Student $studentToMatch): void
+    {
         //check if student has already a mentor
         if ($studentToMatch->getMentoring() === null) {
             //find a mentor with same ProfessionnalSector than the student to match
-            $matchingMentor = $mentorRepository->findOneBy([
+            $matchingMentor = $this->mentorRepository->findOneBy([
                 'professionalSector' => $studentToMatch->getProfessionalSector(),
                 'mentoring' => null]);
 
@@ -30,7 +34,7 @@ class MatchManager
             $mentoring = new Mentoring();
             $mentoring->setStudent($studentToMatch);
             $mentoring->setMentor($matchingMentor);
-            $entityManager->flush();
+            $this->entityManager->flush();
         } else {
             throw new Exception("This student already has an active mentoring");
         }
