ce14ac760215a9e179d3849b62be4f7a3e0aff57
Elodie Daubie <elodie.daubie@gmail.com>
2022-01-13 15:43:13 +0100

    added assert not blank on company

--- src/Entity/Mentor.php
+++ src/Entity/Mentor.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\MentorRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=MentorRepository::class)
@@ -36,6 +37,8 @@ class Mentor
     /**
      * @ORM\ManyToOne(targetEntity=Company::class)
      * @ORM\JoinColumn(nullable=false)
+     * @Assert\NotBlank(
+     *      message = "Veuillez préciser votre entreprise actuelle.")
      */
     private ?Company $company;
 
