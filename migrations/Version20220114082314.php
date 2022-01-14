91f224f01a30e556ef62840fa3675a1025dd0a8a
Thomas905 <thomasclarisse03@gmail.com>
2022-01-14 09:27:13 +0100

    fix migration

--- migrations/Version20220114082314.php
+++ migrations/Version20220114082314.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220112084114 extends AbstractMigration
+final class Version20220114082314 extends AbstractMigration
 {
     public function getDescription(): string
     {
