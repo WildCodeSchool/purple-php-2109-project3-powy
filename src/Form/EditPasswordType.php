f02dcbf97e695136eda798508177538f318fa006
Morgane Lailler <morgane.lailler.pro@gmail.com>
2022-01-16 17:46:45 +0100

    added constraint on password in EditPasswordType

--- src/Form/EditPasswordType.php
+++ src/Form/EditPasswordType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class EditPasswordType extends AbstractType
 {
@@ -32,6 +33,11 @@ class EditPasswordType extends AbstractType
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
+                    new Regex([
+                        'pattern' => '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d](?=.*?([^\w\s]|[_])).{8,}$/',
+                        'message' =>
+                        "Votre mot de passe doit contenir au moins un chiffre, une majuscule et un caractère spécial.",
+                    ])
                 ],
                 'label' => 'Nouveau mot de passe',
             ],
