afbc73acc7156d6309ebbbced664895d35c3ae98
Elodie Daubie <elodie.daubie@gmail.com>
2022-01-17 12:37:29 +0100

    solved conflict with dev, lenght constraint USE was writtent 2 times in MentorType.php

--- src/Form/MentorType.php
+++ src/Form/MentorType.php
@@ -17,7 +17,6 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\Regex;
 
 class MentorType extends AbstractType
