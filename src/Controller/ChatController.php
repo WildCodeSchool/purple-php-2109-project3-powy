5bea103a3defb4bd02ae2c73f1e514bb60257a13
Morgane Lailler <morgane.lailler.pro@gmail.com>
2022-01-21 23:01:35 +0100

    added MessageForm

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use App\Form\MessageType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,7 +17,7 @@ class ChatController extends AbstractController
      * @Route("/chat/{id}", name="chat", requirements={"id"="\d+"})
      * @IsGranted("ROLE_USER")
      */
-    public function index(int $id, UserRepository $userRepository): Response
+    public function index(int $id, UserRepository $userRepository, Request $request): Response
     {
         //add both situations if user is a student or a mentor (because you can only fetch the mentoring this way)
         $user = $userRepository->find($id);
@@ -26,11 +29,14 @@ class ChatController extends AbstractController
                 $mentoring = $user->getStudent()->getMentoring();
             }
         } else {
-            //Had to initialized $mentoring as an empty array to make phpstan happy
+            //Had to initialized $mentoring as an empty array if $user is null to make phpstan happy
             $mentoring = [];
         }
 
         //load the form
+        $message = new Message();
+        $form = $this->createForm(MessageType::class, $message);
+        $form->handleRequest($request);
         //handle the request, etc.
 
         //check if this user has a mentoring, if not redirect to profile
@@ -41,6 +47,7 @@ class ChatController extends AbstractController
 
         return $this->render('chat/index.html.twig', [
             'mentoring' => $mentoring,
+            'messageForm' => $form->createView(),
         ]);
     }
 }
