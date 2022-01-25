57d67a6d35b9d43a9d0cc4bf4a92fed6e9d2be90
Elodie Daubie <elodie.daubie@gmail.com>
2022-01-25 21:23:12 +0100

    creation of method startMentoring

--- src/Service/MentoringManager.php
+++ src/Service/MentoringManager.php
@@ -4,15 +4,26 @@ namespace App\Service;
 
 use App\Entity\Mentoring;
 use App\Entity\User;
+use App\Repository\MentoringRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 
 class MentoringManager
 {
-    /**
-     * This method return if an user (student or mentor) has an active mentoring or not
-     */
+    private MentoringRepository $mentoringRepository;
+    private EntityManagerInterface $entityManager;
 
-    public function checkMentoring(User $user): ?Mentoring
+    public function __construct(MentoringRepository $mentoringRepository, EntityManagerInterface $entityManager)
+    {
+        $this->mentoringRepository = $mentoringRepository;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * return if an user (student or mentor) has an active mentoring or not
+     */
+    public function hasMentoring(User $user): bool
     {
         $mentoring = null;
         if ($user->getMentor() === null && $user->getStudent() === null) {
@@ -23,6 +34,26 @@ class MentoringManager
         } elseif ($user->getStudent() !== null) {
             $mentoring = $user->getStudent()->getMentoring();
         }
-        return $mentoring;
+        if ($mentoring === null) {
+            return false;
+        } elseif ($mentoring->getIsAccepted() === null || $mentoring->getIsAccepted() === false) {
+            return false;
+        }
+        return true;
+    }
+
+    public function startMentoring(Mentoring $mentoring): void
+    {
+        $startingDate = new DateTime();
+        $endingDate = new DateTime();
+        $endingDate->modify('+4 months');
+
+        $mentoringToUpdate = $this->mentoringRepository->find($mentoring);
+        if ($mentoringToUpdate !== null) {
+            $mentoringToUpdate->setIsAccepted(true);
+            $mentoringToUpdate->setStartingDate($startingDate);
+            $mentoringToUpdate->setEndingDtae($endingDate);
+            $this->entityManager->flush();
+        }
     }
 }
