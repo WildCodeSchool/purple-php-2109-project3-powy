c6f289a6cc247f3e91f23ee333f6daf932464b86
Thomas905 <thomasclarisse03@gmail.com>
2022-01-26 18:42:51 +0100

    add route & security & list mentorat and delete option for the mentorat

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,14 +2,19 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\MentoringRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/admin", name="admin_")
+ */
+
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin_app")
+     * @Route("/", name="app")
      */
     public function index(): Response
     {
---
+++ src/Controller/Admin/MentoringController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Mentoring;
+use App\Repository\MentoringRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin", name="admin_")
+ */
+
+class MentoringController extends AbstractController
+{
+
+    /**
+     * @Route("/mentoring", name="mentoring")
+     */
+    public function showMentoring(MentoringRepository $mentoringRepository): Response
+    {
+        $mentoring = $mentoringRepository->findAll();
+        return $this->render('admin/mentoring/index.html.twig', [
+            'mentorings' => $mentoring
+        ]);
+    }
+
+    /**
+     * @Route("/mentoring/{id}", name="mentoring_delete")
+     */
+    public function delete(Request $request, Mentoring $mentoring, EntityManagerInterface $entityManager): Response
+    {
+            $entityManager->remove($mentoring);
+            $entityManager->flush();
+
+        return $this->redirectToRoute('admin_mentoring');
+    }
+}
