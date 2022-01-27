3fbe7636f759383b50c673c1ba83cae3a5549f9a
Elodie Daubie <elodie.daubie@gmail.com>
2022-01-27 14:35:56 +0100

    creation of checkForMatches method

--- src/DataFixtures/MentoringFixtures.php
+++ src/DataFixtures/MentoringFixtures.php
@@ -11,26 +11,32 @@ use Doctrine\Persistence\ObjectManager;
 
 class MentoringFixtures extends Fixture implements DependentFixtureInterface
 {
+    public const MENTORINGFIXTURES = 5;
+
     public function load(ObjectManager $manager): void
     {
+        //set student 0 to 4 with an accepted mentoring
+        for ($i = 0; $i < self::MENTORINGFIXTURES; $i++) {
+            $startingDate = new DateTime('2022-01-20');
+            $mentoring = new Mentoring();
+            $mentoring->setStartingDate($startingDate);
+            $mentoring->setEndingDtae(new DateTime('2022-05-20'));
+            $mentoring->setStudent($this->getReference('student_' . $i));
+            $mentoring->setMentor($this->getReference('mentor_' . $i));
+            $mentoring->setIsAccepted(true);
+            $manager->persist($mentoring);
+        }
+        //set student with a pending mentoring
         $startingDate = new DateTime('2022-01-20');
         $mentoring = new Mentoring();
         $mentoring->setStartingDate($startingDate);
         $mentoring->setEndingDtae(new DateTime('2022-05-20'));
-        $mentoring->setStudent($this->getReference('student_1'));
-        $mentoring->setMentor($this->getReference('mentor_1'));
-        $mentoring->setIsAccepted(true);
-        $manager->persist($mentoring);
-        $manager->flush();
-
-        $startingDate = new DateTime('2022-01-20');
-        $mentoring = new Mentoring();
-        $mentoring->setStartingDate($startingDate);
-        $mentoring->setEndingDtae(new DateTime('2022-05-20'));
-        $mentoring->setStudent($this->getReference('student_2'));
-        $mentoring->setMentor($this->getReference('mentor_2'));
+        $mentoring->setStudent($this->getReference('student_5'));
+        $mentoring->setMentor($this->getReference('mentor_5'));
         $mentoring->setIsAccepted(false);
         $manager->persist($mentoring);
+
+        //flush all user
         $manager->flush();
     }
 
