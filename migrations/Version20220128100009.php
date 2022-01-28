b28c00766c372bc299749be76770460ce82a36eb
Thomas905 <thomasclarisse03@gmail.com>
2022-01-28 11:01:07 +0100

    fix migration

--- migrations/Version20220128100009.php
+++ migrations/Version20220128100009.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220127160911 extends AbstractMigration
+final class Version20220128100009 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20220127160911 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user CHANGE create_at created_at DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE user ADD created_at DATETIME NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user CHANGE created_at create_at DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE user DROP created_at');
     }
 }
